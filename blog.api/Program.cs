ab631d3ff90d3ff64934cf28778c1db05b993164
Sergio Venicius Vanin <sergio.a.vanin@capgemini.com>
2023-12-14 16:26:27 -0300

    trigger config;

--- blog.api/Program.cs
+++ blog.api/Program.cs
@@ -19,6 +19,7 @@ public static class Program
 
         // Add services to the container.
         builder.Services.AddControllers();
+
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen(o =>
